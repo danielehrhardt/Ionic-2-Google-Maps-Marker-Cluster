f8cd0928f2d2519f63412475c59ece7019fffa10
Daniel Ehrhardt <daniel.ehrhardt@bussmann-it.de>
2017-03-27 14:43:41 +0200

    Fixed Toast Bug and updated Ionic to 2.3.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,22 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import {Storage} from '@ionic/storage';
- 
+import { IonicStorageModule } from '@ionic/storage';
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage
   ],
-  providers:  [Storage, {provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
